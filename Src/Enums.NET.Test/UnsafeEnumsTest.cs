358e059eb357f4afa72814a204877adee8417d9c
Tydude4Christ <tydude4christ@hotmail.com>
2016-01-19 12:45:00 -0600

    Updated copyright notice

--- Src/Enums.NET.Test/UnsafeEnumsTest.cs
+++ Src/Enums.NET.Test/UnsafeEnumsTest.cs
@@ -1,5 +1,5 @@
 ﻿// Enums.NET
-// Copyright 2015 Tyler Brinkley. All rights reserved.
+// Copyright 2016 Tyler Brinkley. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
