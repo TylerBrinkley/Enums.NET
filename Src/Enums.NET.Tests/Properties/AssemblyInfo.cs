c2826f1482a4b89df53668b4c63f6216f008e627
TylerBrinkley <Tydude4Christ@hotmail.com>
2017-05-31 00:53:53 -0500

    Beginning Tests work

--- Src/Enums.NET.Tests/Properties/AssemblyInfo.cs
+++ Src/Enums.NET.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Enums.NET.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Enums.NET.Test")]
-[assembly: AssemblyCopyright("Copyright © Tyler Brinkley 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 //[assembly: Guid("4c0acb3b-9ffc-4605-b9ea-1884a0679c3f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
